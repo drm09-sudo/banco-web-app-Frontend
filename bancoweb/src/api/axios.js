fcbdbe37d3d878dba01968f84c49808e994d53f6
David A Recalde Mendez <david@MacBook-Air-de-David.local>
2026-09-23 20:34:30 -0500

    fix: actualizar la url de la api a render

--- bancoweb/src/api/axios.js
+++ bancoweb/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api'
+    baseURL: 'https://mi-banco-backend.onrender.com'
 });
 
 //Creo el interceptor para adjuntar automaticamente el token
